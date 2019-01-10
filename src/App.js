c5b31f8c670faa33c978672a49e3216641d2d84b
Joanna Filipowska <j.filipowska@gmail.com>
2019-01-09 20:03:06 -0600

    test app3

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="right">
+        <p>RIGHT ANSWER </p>
+        </div>
+        <div className="wrong">
+        <p>Wrong Answer </p>
+        </div>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num, i) => {
+          return {
+            key={i}
+            onClick={() => this.guessNumber(num)}
+            className="num-box"
+            >
+            <p>(num)</p>
+          }
+        })}
       </div>
     );
   }
