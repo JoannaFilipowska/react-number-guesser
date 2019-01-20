bc941aabf96d2a47d5d992194aae77334cada4cf
Joanna Filipowska <j.filipowska@gmail.com>
2019-01-19 21:53:58 -0500

    homework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      random: null
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ random: Math.floor(Math.random() * 10) + 1 });
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="right">
-        <p>RIGHT ANSWER </p>
-        </div>
-        <div className="wrong">
-        <p>Wrong Answer </p>
-        </div>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num, i) => {
-          return {
-            key={i}
-            onClick={() => this.guessNumber(num)}
-            className="num-box"
-            >
-            <p>(num)</p>
-          }
-        })}
+      <div>
+        <button onClick={this.handleClick}>Click me</button>
+        {this.state.random}
       </div>
     );
   }
